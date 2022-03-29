6df943af0e1d1cc38c5003e000986f8556cbb6b8
James Grooms <80604695+JamesGrooms@users.noreply.github.com>
2022-03-29 14:56:33 -0400

    Add files via upload

---
+++ image_upload.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from keras.preprocessing import image
+from keras.models import model_from_json
+import io
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Pick an image to test')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        original_image = Image.open(uploaded_file)
+        original_image = np.array(original_image)
+        return original_image
+    else:
+        return None
+    
+
+        
+def load_model():
+    json_file = open('C:\\Users\\groom\\Desktop\\model (2).json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("C:\\Users\\groom\\Desktop\\model_best_weights (2).h5")
+    return loaded_model
+
+def load_labels(labels_file):
+    with open(labels_file, "r") as f:
+        categories = [s.strip() for s in f.readlines()]
+        return categories
+
+def predict(loaded_model, categories, x):
+    labels = categories
+    width = 224
+    height = 224
+    dim = (width, height)
+    #import numpy as np 
+    #image = np.array(image)
+    #img = cv2.resize(image, dim,interpolation = cv2.INTER_LINEAR)
+
+    #img = image.load_img(target_size=(224, 224))
+    #x = image.img_to_array(image)
+    x = cv2.resize(x, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+    x = np.expand_dims(x, axis=0) /255
+    classes = loaded_model.predict(x)
+    st.write("Closed:", np.argmax(classes[0])==0, (classes[0][0]), round(classes[0][0]) * 100,"%") # 0 : Eyes closed
+    st.write("Open:" , np.argmax(classes[0])==1, (classes[0][1]), round(classes[0][1]) * 100,"%") # 1 : Eyes open
+    st.write("No-yawn:" , np.argmax(classes[0])==2, (classes[0][2]), round(classes[0][2])  * 100,"%")# 2 : No yawn
+    st.write("Yawn:" , np.argmax(classes[0])==3, (classes[0][3]), round(classes[0][3])  * 100,"%")# 3 : Yawn
+    st.write(f"Predicted label: {labels[np.argmax(classes[0])]}") # Predicted label
+    st.write(f"Probability of prediction): {round(np.max(classes[0])) * 100} %") # Probablilty of prediction
+
+
+
+
+def main():
+    
+    LABELS_PATH = 'C:\\Users\\groom\\Desktop\\model_classes.txt'
+    st.title('Pretrained model demo')
+    model = load_model()
+    categories = load_labels(LABELS_PATH)
+    image = load_image()
+    result = st.button('Run on image')
+    if result:
+        st.write('Calculating results...')
+        predict(model, categories, image)
+
+
+if __name__ == '__main__':
+    main()
